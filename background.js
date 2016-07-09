f26a2e0114a9a7b2fdae411e82e32c5dc764d1db
ErikBoesen <me@erikboesen.com>
2016-07-09 11:35:20 -0400

    A few comments, cleaning

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-   chrome.tabs.executeScript(null, {file: 'open.js'});
+    chrome.tabs.executeScript(null, {file: 'open.js'});
 });
--- open.js
+++ open.js
@@ -1,5 +1,7 @@
+// Make array of all post title links
 var l = document.querySelectorAll('a.title');
 
+// Go through array, and open each of the links in a new tab.
 for (i = 0; i < l.length; i++) {
     window.open(l[i].href);
 }
